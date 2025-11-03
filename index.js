e7d567b85babc36fd24d7be2e822f56bb88ac6ff
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-11-03 12:10:54 +0300

    10.4

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ if (promocode != null && promocode.toLowerCase()==="скидка") {
 else {
     alert('Промокод не работает');
 }
+
+//Задание 10.3
+const name = prompt("Ваше имя: ");
+const year = prompt("Год вашего рождения: ");
+let date= new Date();
+let res = date.getFullYear();
+alert(`Ваше имя ${name}, возраст: ${res - year} `);
+
+//Задание 10.4
+let username = prompt("Ваше имя: ");
+let inputfBirth = prompt("Год вашего рождения: ");
+let yearofBirth= +inputfBirth;
+let dateNow= new Date();
+let res_1 = dateNow.getFullYear();
+let yourage = res_1 - yearofBirth;
+let ost = yourage % 10;
+if (ost === 1) {
+    alert("_1год");
+} else if (ost === 2 || ost === 3 || ost === 4) {
+    alert("_3года");
+} else if(ost === 5 || ost === 6 || ost === 7 || ost === 8 || ost ===9 ||
+    ost === 0) {
+    alert("_7 лет");
+}
+else {
+    alert("Год не число или пустое имя.");
+}
