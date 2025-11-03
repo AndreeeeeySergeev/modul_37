87dc75c89ebf44f4491eecf7e9a63a9dcc250104
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-11-03 10:38:55 +0300

    10.1 10.2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+//Задание 10.1
+let ask = prompt("Введите число: ");
++ask;
+alert(`Ваше число: ${ask};\n Квадрат вашего числа ${ask ** 2};\n
+       Куб вашего числа ${ask ** 3} `);
+
+//Задание 10.2
+let promocode = prompt("Введите ваш промокод: ");
+
+if (promocode != null && promocode.toLowerCase()==="скидка") {
+    alert("Промокдо применён");
+}
+else {
+    alert('Промокод не работает');
+}
